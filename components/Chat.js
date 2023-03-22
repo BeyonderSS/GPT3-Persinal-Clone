bfed0f78944e954d3974b0c0b2a58fb655a22e7a
BeyonderSS <ssbeyonder@gmail.com>
2023-03-22 22:28:17 +0530

    main

--- components/Chat.js
+++ components/Chat.js
@@ -1,31 +1,67 @@
-import { useState } from 'react';
-import { FiChevronsRight } from 'react-icons/fi';
+import { useState } from "react";
+import { FiChevronsRight } from "react-icons/fi";
+import fetch from "isomorphic-unfetch";
 const Chat = () => {
   const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (inputValue.trim() !== '') {
-      setMessages((prevState) => [...prevState, { message: inputValue, isUser: true }]);
-      setInputValue('');
+    if (inputValue.trim() !== "") {
+      // add the user's message to the messages array
+      setMessages((prevState) => [
+        ...prevState,
+        { message: inputValue, isUser: true },
+      ]);
+      setInputValue("");
+
+      try {
+        // define the request body
+        const requestBody = {
+          prompt: inputValue,
+          max_tokens: 50,
+          stop: "The end",
+          temperature: 0.9,
+        };
+        // send the request to the API
+        const response = await fetch("/api/openai", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        });
+        const data = await response.json();
+
+        // add the AI's response to the messages array
+        setMessages((prevState) => [
+          ...prevState,
+          { message: data.choices[0].text, isUser: false },
+        ]);
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
   return (
     <div className="bg-[#313131] min-h-screen pb-20">
-     
       <div className="p-4">
         <ul>
           {messages.map((msg, index) => (
-            <li key={index} className={`${msg.isUser ? 'text-right' : ''} mb-4`}>
+            <li
+              key={index}
+              className={`${msg.isUser ? "text-right" : ""} mb-4`}
+            >
               <div
                 className={`px-4 py-2 rounded-lg inline-block ${
-                  msg.isUser ? 'bg-gray-700 text-white' : 'bg-blue-600 text-white'
+                  msg.isUser
+                    ? "bg-gray-700 text-white"
+                    : "bg-blue-600 text-white"
                 }`}
               >
                 {msg.message}
@@ -34,7 +70,10 @@ const Chat = () => {
           ))}
         </ul>
       </div>
-      <form onSubmit={handleSubmit} className="fixed bottom-0 left-0 w-full bg-[#242424] p-4 flex">
+      <form
+        onSubmit={handleSubmit}
+        className="fixed bottom-0 left-0 w-full bg-[#242424] p-4 flex"
+      >
         <input
           type="text"
           value={inputValue}
